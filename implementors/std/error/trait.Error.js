9cff9d7d656838954902eb1f06f8ec89d49e1f6b
jhpratt <the.z.cuber@gmail.com>
2020-09-26 01:26:00 +0000

    Deploying to gh-pages from  @ ad4740f36895130e33fcf8eddee54cc91ef4a57c 🚀

---
+++ implementors/std/error/trait.Error.js
@@ -0,0 +1,5 @@
+(function() {var implementors = {};
+implementors["serde"] = [{"text":"impl Error for Error","synthetic":false,"types":[]}];
+implementors["syn"] = [{"text":"impl Error for Error","synthetic":false,"types":[]}];
+implementors["time"] = [{"text":"impl Error for Error","synthetic":false,"types":[]},{"text":"impl Error for ConversionRange","synthetic":false,"types":[]},{"text":"impl Error for ComponentRange","synthetic":false,"types":[]},{"text":"impl Error for IndeterminateOffset","synthetic":false,"types":[]},{"text":"impl Error for Format","synthetic":false,"types":[]},{"text":"impl Error for Error","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
